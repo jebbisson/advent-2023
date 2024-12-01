b7014475dfd2f0adbc7bebdbcf1b432d3b33a678
JebBisson <jeb.bisson@gmail.com>
2024-11-30 22:09:30 -0500

    second part finished

--- 1/main.go
+++ 1/main.go
@@ -23,6 +23,19 @@ func main() {
 
 	total := 0
 
+	words := map[string]int{
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
 	for scanner.Scan() {
 		textLn := scanner.Text()
 		log.Printf("S: %v ", textLn)
@@ -32,20 +45,41 @@ func main() {
 		first := strings.IndexAny(textLn, "0123456789")
 		last := strings.LastIndexAny(textLn, "0123456789")
 
+		firstNum, lastNum := "", ""
+		if first != -1 {
+			firstNum = string(textLn[first])
+		}
+		if last != -1 {
+			lastNum = string(textLn[last])
+		}
+
+		for key, val := range words {
+			temp := strings.Index(textLn, key)
+			if temp != -1 && (temp < first || first == -1) {
+				first = temp
+				firstNum = string(strconv.Itoa(val)) //this is innefficient
+			}
+			if first == 0 {
+				break
+			}
+		}
+
+		for key, val := range words {
+			temp := strings.LastIndex(textLn, key)
+			if temp > last {
+				last = temp
+				lastNum = string(strconv.Itoa(val))
+			}
+			if last == len(textLn)-1 { //not sure if its -1 or -2
+				break
+			}
+		}
+
 		if first == -1 { //for now I dont think instructions say to ignore lines without numbers
 			log.Fatal("No number found!")
 		}
 
-		//example show this as valid
-		//if first == last {
-		//	log.Fatal("First and last index are the same: %v %v", first, last)
-		//}
-
-		log.Printf("I: %v,%v", first, last)
-
-		firstNum := string(textLn[first])
-		lastNum := string(textLn[last])
-		log.Printf("C: %v,%v", firstNum, lastNum)
+		log.Printf("I: %v,%v C: %v,%v", first, last, firstNum, lastNum)
 
 		num, err := strconv.Atoi(string(firstNum + lastNum))
 
@@ -54,7 +88,6 @@ func main() {
 		}
 
 		log.Printf(" N:%v T:%v", num, total)
-
 		log.Println()
 
 		total += num
